fe80ea4d32e411b84ce1496d80d1b71945e33f75
Stefan Cvetanovski <stefan.cvetanovski@greenassetswallet.org>
2022-03-21 20:39:27 +0100

    Second commit

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,32 +11,19 @@ const dropdownList = [
 ];
 const Dropdown = () => {
 	const [selected, setSelected] = useState(dropdownList[0].value);
-	const [isOpen, setIsOpen] = useState(false);
 	const refContainer = useRef(null);
 	console.log(selected);
 
-	useEffect(() => {
-		window.addEventListener('orientationchange', (event) => {
-			setIsOpen(false);
-      setSelected(dropdownList[0].value);
-		});
-		return () => {};
-	}, []);
-  	console.log(isOpen);
+
 	return (
 		<div className={styles.root}>
 			<select
 				ref={refContainer}
 				className={styles.dropdown}
 				onChange={(e) => setSelected(e.target.value)}
-        onClick={()=>setIsOpen(true)}
 			>
-				{dropdownList.map(({ option, value },i) => {
-					return isOpen && i > 0 ? (
-						<option value={value}>{option}</option>
-					) : (
-						<option value={value}>{option}</option>
-					);
+				{dropdownList.map(({ option, value }) => {
+					return (<option value={value}>{option}</option>) 
 				})}
 			</select>
 		</div>
